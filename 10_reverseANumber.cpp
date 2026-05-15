7bce42edcffb66be93d2f12d4b51ca3d9a49ff4a
Israr-1729 <284752342+Israr-1729@users.noreply.github.com>
2026-05-15 17:35:30 +0530

    Create 10_reverseANumber.cpp

---
+++ 10_reverseANumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    int num;
+    std::cout<<"Enter the number you want to reverse \n";
+    std::cin>>num;
+    long long reverse = 0;
+    int copy = num;
+    while(copy!=0)
+    {
+        reverse = reverse*10 + copy%10;
+        copy/=10;
+    }
+    std::cout<<"The reverse of "<<num<<" is "<<reverse;
+}
