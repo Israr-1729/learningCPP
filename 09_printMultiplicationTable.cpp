6c0f4e0d93f41a3e4c9280fa9d27c26bf62379a9
Israr-1729 <284752342+Israr-1729@users.noreply.github.com>
2026-05-15 17:22:01 +0530

    Create 09_printMultiplicationTable.cpp

---
+++ 09_printMultiplicationTable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int num;
+    std::cout<<"Enter the number whose multiplication table you need: ";
+    std::cin>>num;
+    for(int i = 1; i<11; i++)
+    {
+        std::cout<<num<<" x "<<i<<"\t=\t"<<num*i<<"\n";
+    }
+}
