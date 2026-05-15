ec3b24755133e75adfce5cc9a64c8fdefdbeb7b7
Israr-1729 <284752342+Israr-1729@users.noreply.github.com>
2026-05-15 17:40:37 +0530

    Create 11_signum.cpp

---
+++ 11_signum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    int num;
+    std::cout<<"Enter the number you wish to check: ";
+    std::cin>>num;
+
+    if(num>0)
+    std::cout<<"+\n";
+    else if(num<0)
+    std::cout<<"-\n";
+    else
+    std::cout<<"0\n";
+}
